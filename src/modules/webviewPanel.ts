b049dfb168ec0af6871f357dedd69fd238b236bb
taylorychen <tchen073@gmail.com>
2025-02-06 00:51:06 -0800

    add icon

--- src/modules/webviewPanel.ts
+++ src/modules/webviewPanel.ts
@@ -97,11 +97,7 @@ export class TypingTestPanel {
         const webview = this._panel.webview;
         this._panel.webview.html = this.webViewHtml(webview);
         this._panel.title = TypingTestPanel.viewTitle;
-        // this._panel.iconPath = vscode.Uri.joinPath(
-        //   this._extensionUri,
-        //   "webview",
-        //   "icon.svg"
-        // );
+        this._panel.iconPath = vscode.Uri.joinPath(this._extensionUri, "webview", "yinyang.svg");
     }
 
     public dispose() {
@@ -149,7 +145,6 @@ export class TypingTestPanel {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="${stylesUri}">
-
 
         <title>Code Monk</title>
 
