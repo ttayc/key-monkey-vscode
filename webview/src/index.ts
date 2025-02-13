9c912cedeeeacf567cf1333d503686af8da68ee0
Vincent Lin <vinlin24@outlook.com>
2025-02-12 19:32:10 -0800

    feat: add key listeners for restart/next/retry

--- webview/src/index.ts
+++ webview/src/index.ts
@@ -7,9 +7,14 @@ const vscode = acquireVsCodeApi();
 
 // tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// @ts-expect-error
 const _ = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
 
 let currentPassage: string[];
+
+function startNextTest() {
+  getNewPassage(getTypingTestConfig());
+}
 
 document.getElementById("restart-button")?.addEventListener("click", (_) => {
   resetOrRestartTest();
@@ -20,9 +25,47 @@ document.getElementById("retry-button")?.addEventListener("click", (_) => {
 });
 
 document.getElementById("next-button")?.addEventListener("click", (_) => {
-  getNewPassage(getTypingTestConfig());
+  startNextTest();
 });
 
+document.addEventListener("keydown", (event) => {
+  // Key mappings:
+  //
+  //    - ENTER => equivalent to clicking next-button
+  //    - SHIFT-ENTER => equivalent to clicking restart-button OR retry-button
+  //
+  // For consistency, we check that the respective button is available for the
+  // key bind to be considered active. This is to respect the existing UI where
+  // certain buttons are only available on certain "pages", which are
+  // ever-present in the DOM but take turns being visible. This hopefully makes
+  // any updates to such buttons automatically propagate to this listener.
+
+  function isElementAvailable(id: string): boolean {
+    const element = document.getElementById(id);
+    if (!element) {
+      return false;
+    }
+    // The buttons may not be directly `.hidden` (but rather indirectly e.g.
+    // next-button via the results page), so we use `.offsetParent` instead:
+    // https://stackoverflow.com/a/21696585/14226122.
+    return element.offsetParent !== null;
+  }
+
+  const { key, shiftKey } = event;
+  if (key === "Enter" && shiftKey && isElementAvailable("restart-button")) {
+    console.log("Restarting test via keybind");
+    resetOrRestartTest();
+  }
+  if (key === "Enter" && shiftKey && isElementAvailable("retry-button")) {
+    console.log("Retrying test via keybind");
+    resetOrRestartTest();
+  }
+  if (key === "Enter" && !shiftKey && isElementAvailable("next-button")) {
+    console.log("Moving on to next test via keybind");
+    startNextTest();
+  }
+  // Other potential key mappings...
+});
 
 export function getNewPassage(config: TypingTestConfig) {
   console.log('get new passage');
